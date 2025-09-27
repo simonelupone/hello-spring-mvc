f72cebe8dd98b622f9c570d68f1b8dbd01822321
simonelupone <simone.lupone@gmail.com>
2025-09-27 14:53:22 +0200

    first commit

---
+++ src/main/java/com/java/ex/spring/hello_spring_mvc/HelloSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.java.ex.spring.hello_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/ex/spring/hello_spring_mvc/HelloSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ex.spring.hello_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
